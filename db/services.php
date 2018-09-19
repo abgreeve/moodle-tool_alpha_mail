d269ba848cb45189432e19b18aca0f081f3c96d5
Cameron Ball <cameron@moodle.com>
2018-09-19 14:07:19 +0800

    Implement a webservice to retrieve the list of notifications
    Relevent documentation:
    
     - https://docs.moodle.org/dev/Web_services
     - https://docs.moodle.org/dev/AJAX

---
+++ db/services.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * External functions and service definitions.
+ *
+ * @package    tool_alpha_mail
+ * @copyright  2018 Cameron Ball <cameron@cameron1729.xyz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$functions = [
+    'tool_alpha_mail_get_popup_messages' => [
+        'classname' => 'tool_alpha_mail_external',
+        'methodname' => 'get_popup_messages',
+        'description' => 'Retrieve a list of messages from the admin',
+        'type' => 'read',
+        'ajax' => true,
+    ]
+];
---
+++ externallib.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Alpha Mail external APIs.
+ *
+ * @package    tool_alpha_mail
+ * @copyright  2018 Cameron Ball <cameron@cameron1729.xyz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir . '/externallib.php');
+
+class tool_alpha_mail_external extends external_api {
+    /**
+     * Get popup messages from the site admin.
+     *
+     * @return string
+     */
+    public static function get_popup_messages() {
+        global $DB, $USER;
+
+        self::validate_context(context_system::instance());
+        $messages = array_values($DB->get_records('tool_alpha_mail_messages', ['userid' => $USER->id]));
+
+        return $messages;
+    }
+
+    protected static function get_popup_messages_returns() {
+        return new external_multiple_structure(
+            new external_single_structure(
+                [
+                    'body' => new external_value(PARAM_TEXT, 'the message body')
+                ]
+            )
+        );
+    }
+
+    protected static function get_popup_messages_parameters() {
+        return new external_function_parameters([]);
+    }
+}
