eed0d38a7ba80d91802d46f57ae547036a127d34
Cameron Ball <cameron@moodle.com>
2018-09-20 16:11:59 +0800

    Run grunt and bump version

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018091200;            // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2018092000;            // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018090700;            // Requires this Moodle version.
 $plugin->component = 'tool_alpha_mail';      // Full name of the plugin (used for diagnostics).
